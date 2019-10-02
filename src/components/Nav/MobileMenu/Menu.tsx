5ee492c39233868c934bf4e4656d7a20f457f3ce
Burak <brkcrdk@gmail.com>
2019-10-02 17:12:30 +0300

    Product type function added

--- src/components/Nav/MobileMenu/Menu.tsx
+++ src/components/Nav/MobileMenu/Menu.tsx
@@ -1,13 +1,12 @@
 import React from "react";
-import { IProducts } from "../../../store/fetchProducts/types";
+import getType from "../../getType";
+// import { IProducts } from "../../../store/fetchProducts/types";
 
 import { useSelector } from "react-redux";
 
 interface ProductTypes {
   storeProducts: {
     products: [];
-    isLoading: boolean;
-    error: null | {};
   };
 }
 
@@ -15,18 +14,19 @@ const Menu: React.FC = () => {
   const products = useSelector(
     (state: ProductTypes) => state.storeProducts.products
   );
-  const productTypes = products
-    .filter((items: IProducts) => {
-      return items.brand;
-    })
-    .map((x: IProducts) => {
-      return x.brand;
-    });
-  //TODO: Filter to get array without duplicates
-  const types = productTypes.filter((value, index, self) => {
-    return self.indexOf(value) === index;
-  });
-  console.log(types);
+  console.log(getType("brand", products));
+  // const productTypes = products
+  //   .filter((items: IProducts) => {
+  //     return items.brand;
+  //   })
+  //   .map((x: IProducts) => {
+  //     return x.brand;
+  //   });
+  // //TODO: Filter to get array without duplicates
+  // const types = productTypes.filter((value, index, self) => {
+  //   return self.indexOf(value) === index;
+  // });
+  // console.log(types);
   return (
     <ul>
       <li>Menu</li>
