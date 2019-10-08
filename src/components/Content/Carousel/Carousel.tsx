8a0640947e33b065abd2257f3877685c351e3e8e
Burak <brkcrdk@gmail.com>
2019-10-08 20:55:04 +0300

    Carousel function added

--- src/components/Content/Carousel/Carousel.tsx
+++ src/components/Content/Carousel/Carousel.tsx
@@ -1,47 +1,48 @@
 import React, { useState, useEffect } from "react";
 
 import { CarouselContainer, CarouselContent } from "./CarouselStyle";
-
+const items = [
+  [{ name: "Burak1" }, { adres: "Muğla1" }],
+  [{ name: "Burak2" }, { adres: "Muğla2" }],
+  [{ name: "Burak3" }, { adres: "Muğla3" }],
+  [{ name: "Burak4" }, { adres: "Muğla4" }],
+  [{ name: "Burak5" }, { adres: "Muğla5" }]
+];
 interface Props {}
 
 const Carousel: React.FC<Props> = () => {
-  const [wSize, setWSize] = useState(0);
+  // const [wSize, setWSize] = useState(0);
+  const grid = 2;
+  // useEffect(() => {
+  //   window.addEventListener("resize", () => {
+  //     if (window.innerWidth !== wSize) {
+  //       return setWSize(window.innerWidth);
+  //     } else {
+  //       return wSize;
+  //     }
+  //   });
+  // }, []);
+  // if (wSize === 320) {
+  //   console.log("mobileS");
+  // } else if (wSize > 320 && wSize <= 375) {
+  //   console.log("mobileM");
+  // } else if (wSize > 375 && wSize <= 425) {
+  //   console.log("MobileL");
+  // }
 
-  useEffect(() => {
-    window.addEventListener("resize", () => {
-      if (window.innerWidth !== wSize) {
-        return setWSize(window.innerWidth);
-      } else {
-        return wSize;
-      }
-    });
-  }, []);
-  if (wSize === 320) {
-    console.log("mobileS");
-  } else if (wSize > 320 && wSize <= 375) {
-    console.log("mobileM");
-  } else if (wSize > 375 && wSize <= 425) {
-    console.log("MobileL");
-  }
   return (
     <CarouselContainer>
-      <CarouselContent>
-        <li>Eleman1</li>
-        <li>Eleman2</li>
-        <li>Eleman3</li>
-      </CarouselContent>
-      <CarouselContent>
-        <li>Eleman3</li>
-        <li>Eleman4</li>
-      </CarouselContent>
-      <CarouselContent>
-        <li>Eleman5</li>
-        <li>Eleman6</li>
-      </CarouselContent>
-      <CarouselContent>
-        <li>Eleman7</li>
-        <li>Eleman8</li>
-      </CarouselContent>
+      {items.map((array, key) => (
+        <CarouselContent>
+          {array.map((item, key) => {
+            if (!item.name) {
+              return <li>{item.adres}</li>;
+            } else if (!item.adres) {
+              return <li>{item.name}</li>;
+            }
+          })}
+        </CarouselContent>
+      ))}
     </CarouselContainer>
   );
 };
