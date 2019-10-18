88bcf094bb80a58b6bbf0e392dcc3e1387535faa
Burak <brkcrdk@gmail.com>
2019-10-18 19:12:54 +0300

    Cleaning

--- src/components/Content/SecondSection/MostWanted/Cards/Cards.tsx
+++ src/components/Content/SecondSection/MostWanted/Cards/Cards.tsx
@@ -3,9 +3,24 @@ import CardsDesktop from "./CardsDesktop";
 import CardsMobile from "./CardsMobile";
 import CardsMobileL from "./CardsMobileL";
 import CardsTablet from "./CardsTablet";
-interface Props {}
+import { randomItems } from "../../../../getTypes";
+import handleProducts from "../handleProducts";
+import { useSelector } from "react-redux";
+interface ProductProps {
+  storeProducts: {
+    products: [];
+  };
+}
 
-const Cards: React.FC<Props> = () => {
+const Cards: React.FC = () => {
+  const products = useSelector(
+    (state: ProductProps) => state.storeProducts.products
+  );
+  const randomProducts = randomItems(products);
+  console.log(handleProducts(randomProducts, 2));
+  //TODO: For mobileS : 1 , mobileM: 1, mobileL: 2
+  //TODO: For mobileTablet : 2 ,tablet:3,laptop 3,
+  //TODO: For desktop and desktopL: 4
   return (
     <>
       <CardsDesktop />
--- src/components/Content/SecondSection/MostWanted/Wanted.tsx
+++ src/components/Content/SecondSection/MostWanted/Wanted.tsx
@@ -1,26 +1,9 @@
 import React from "react";
 import { Container, Header } from "./WantedStyle";
-import { randomItems } from "../../../getTypes";
-import handleProducts from "./handleProducts";
-import { useSelector } from "react-redux";
+
 import Cards from "./Cards/Cards";
-// import Carousel from "../../../UI/Carousel/Carousel";
-// import Card from "../../../UI/Card/Card";
-interface ProductProps {
-  storeProducts: {
-    products: [];
-  };
-}
 
 const Wanted: React.FC = () => {
-  const products = useSelector(
-    (state: ProductProps) => state.storeProducts.products
-  );
-  const randomProducts = randomItems(products);
-  console.log(handleProducts(randomProducts, 2));
-  //TODO: For mobileS : 1 , mobileM: 1, mobileL: 2
-  //TODO: For mobileTablet : 2 ,tablet:3,laptop 3,
-  //TODO: For desktop and desktopL: 4
   return (
     <Container>
       <Header>Most Search Items</Header>
