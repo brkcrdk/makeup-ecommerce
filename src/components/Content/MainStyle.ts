5ff3ccfa0390e1310029c14ead7e276c62105c72
Burak <brkcrdk@gmail.com>
2019-10-22 17:34:16 +0300

    Cleaning and fix

--- src/components/Content/MainStyle.ts
+++ src/components/Content/MainStyle.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import background from "./makeup.jpg";
+import background from "./mainPic.jpg";
 import colours from "../colours";
 
 export const Container = styled.div``;
