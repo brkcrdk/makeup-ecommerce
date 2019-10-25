fd6a79150fa291331ea36389667adf601f6901d2
Burak <brkcrdk@gmail.com>
2019-10-25 20:54:48 +0300

    Pagination for shop page

--- src/components/Shop/ProductList/ProductList.tsx
+++ src/components/Shop/ProductList/ProductList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { IProduct } from "../../../store/fetchProduct/types";
 interface Props {
   products: IProduct[];
@@ -6,11 +6,21 @@ interface Props {
 }
 
 const ProductList: React.FC<Props> = ({ products, isLoading }) => {
+  const [itemPerPage, setItemPerPage] = useState(15);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const indexOfLastItem = currentPage * itemPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemPerPage;
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(products.length / itemPerPage); i++) {
+    pageNumbers.push(i);
+  }
+  console.log(pageNumbers);
   if (isLoading) return <p>Loading...</p>;
   return (
     <div>
       {products.map((product, index) => (
-        <li>{product.name}</li>
+        <li key={index}>{product.name}</li>
       ))}
     </div>
   );
