55cbe9f2e116487289ae07b4f89004354fbe76d6
Burak <brkcrdk@gmail.com>
2019-10-28 14:05:27 +0200

    Cleaning

--- src/components/Shop/ProductList/ProductStyle.ts
+++ src/components/Shop/ProductList/ProductStyle.ts
@@ -5,6 +5,12 @@ export const Container = styled.div`
   margin: 0 1em;
   
   overflow:hidden;
+   @media ${device.mobileL}{
+    padding-right:0.5em;
+  }
+   @media ${device.laptop}{
+    padding-right:1em;
+  }
   @media ${device.laptopL}{
     padding-right:2em;
   }
