b079de309f08da2f839078fc424449805755c4be
Burak <brkcrdk@gmail.com>
2019-10-28 16:38:42 +0200

    Range slider

--- src/components/Shop/Filter/Filter.tsx
+++ src/components/Shop/Filter/Filter.tsx
@@ -3,24 +3,43 @@ import { Container } from "./FilterStyle";
 interface Props {}
 
 const Filter: React.FC<Props> = () => {
-  const [max, setMax] = useState(0);
   const [min, setMin] = useState(0);
-  const handleMax = () => {};
-  const handleMin = () => {};
-  const slider = React.createElement("input", { type: "range" });
+  const [max, setMax] = useState(100);
+  const handleMin = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = parseFloat(e.target.value);
+    if (value >= 0 && value <= 100) {
+      setMin(value);
+    } else {
+      alert("value must be 0-100");
+    }
+  };
+
+  const handleMax = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = parseFloat(e.target.value);
+    if (value >= 0 && value <= 100) {
+      setMin(value);
+    } else {
+      alert("value must be 0-100");
+    }
+  };
+  const slider = React.createElement("input", {
+    type: "range",
+    max: "100",
+    min: 0,
+    step: 1,
+    value: "10"
+  });
 
   return (
     <Container>
       <h3>Filter By</h3>
       <hr />
-      <form>
-        <div style={{ display: "flex" }}>
-          <input type="text" data-index="0" value={min} onChange={handleMin} />
-          <input type="text" data-index="1" value={max} onChange={handleMax} />
-        </div>
-        <br />
-        <div id="slider">{slider}</div>
-      </form>
+      <div>
+        <span className="multi-range">
+          <input type="range" min="0" max="50" value="5" id="lower" />
+          <input type="range" min="0" max="50" value="45" id="upper" />
+        </span>
+      </div>
     </Container>
   );
 };
