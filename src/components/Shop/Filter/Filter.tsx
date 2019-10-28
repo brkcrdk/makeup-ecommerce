e28d41b81d6623f8b5259ffbd1bd535a8b0e9f9a
Burak <brkcrdk@gmail.com>
2019-10-28 14:56:06 +0200

    Multiple range slider added

--- src/components/Shop/Filter/Filter.tsx
+++ src/components/Shop/Filter/Filter.tsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container } from "./FilterStyle";
 interface Props {}
 
 const Filter: React.FC<Props> = () => {
-  return <Container>Filter section</Container>;
+  const [max, setMax] = useState(0);
+  const [min, setMin] = useState(0);
+  const slider = React.createElement("input", { type: "range" });
+  return (
+    <Container>
+      <h3>Filter By</h3>
+      <hr />
+      <form>
+        <div style={{ display: "flex" }}>
+          <input type="text" data-index="0" value={max} />
+          <input type="text" data-index="1" value={min} />
+        </div>
+        <br />
+        <div id="slider">{slider}</div>
+      </form>
+    </Container>
+  );
 };
 
 export default Filter;
--- src/components/Shop/Filter/FilterStyle.tsx
+++ src/components/Shop/Filter/FilterStyle.tsx
@@ -2,4 +2,5 @@ import styled from "styled-components";
 import { colours } from "../../utils";
 export const Container = styled.div`
   /* box-shadow: ${colours.boxShadow}; */
+  text-align:center;
 `;
