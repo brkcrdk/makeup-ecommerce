7754a3a5e4332dec48feb0ff70b763c1adde94bc
Burak <brkcrdk@gmail.com>
2019-10-28 14:58:04 +0200

    Range slider

--- src/components/Shop/Filter/Filter.tsx
+++ src/components/Shop/Filter/Filter.tsx
@@ -5,15 +5,18 @@ interface Props {}
 const Filter: React.FC<Props> = () => {
   const [max, setMax] = useState(0);
   const [min, setMin] = useState(0);
+  const handleMax = () => {};
+  const handleMin = () => {};
   const slider = React.createElement("input", { type: "range" });
+
   return (
     <Container>
       <h3>Filter By</h3>
       <hr />
       <form>
         <div style={{ display: "flex" }}>
-          <input type="text" data-index="0" value={max} />
-          <input type="text" data-index="1" value={min} />
+          <input type="text" data-index="0" value={min} onChange={handleMin} />
+          <input type="text" data-index="1" value={max} onChange={handleMax} />
         </div>
         <br />
         <div id="slider">{slider}</div>
