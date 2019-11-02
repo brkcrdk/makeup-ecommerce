350e80f278a40001d782508461a99da039c9b7ab
Burak <brkcrdk@gmail.com>
2019-11-02 17:51:01 +0200

    Bug fix

--- src/components/Shop/Filter/FilterTypes/Brands.tsx
+++ src/components/Shop/Filter/FilterTypes/Brands.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { getBrand } from "../../../getTypes";
 import { useDispatch } from "react-redux";
+import { searchFilter } from "../../../../store/searchFilter/action";
 import Selectbox from "../../../UI/Selectbox/Selectbox";
 interface Props {
   products: [];
@@ -14,7 +15,6 @@ const Brands: React.FC<Props> = ({ products, isLoading }) => {
       options={brands}
       label="Brands :"
       defaultValue={`${isLoading ? "Loading.." : "Choose here."}`}
-      onChange={}
     />
   );
 };
