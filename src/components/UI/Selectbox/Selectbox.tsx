75b9e5c5dc69da41a89560631d91491356ee1906
Burak <brkcrdk@gmail.com>
2019-11-02 17:01:58 +0200

    Selectbox component style change

--- src/components/UI/Selectbox/Selectbox.tsx
+++ src/components/UI/Selectbox/Selectbox.tsx
@@ -5,6 +5,18 @@ interface Props {
   label: string;
   defaultValue?: string;
 }
+const Container = styled.div`
+  label {
+    margin-bottom: 0.5em;
+    font-weight: 600;
+  }
+  select {
+    width: 100%;
+    padding: 0.5em;
+    margin-bottom: 1em;
+    font-size: 1em;
+  }
+`;
 
 const Selectbox: React.FC<Props> = ({
   defaultValue = "Choose here",
@@ -12,8 +24,9 @@ const Selectbox: React.FC<Props> = ({
   label
 }) => {
   return (
-    <>
+    <Container>
       <label>{label}</label>
+      <hr />
       <select>
         <option value="">{defaultValue}</option>
         {options.map((option, index) => (
@@ -22,7 +35,7 @@ const Selectbox: React.FC<Props> = ({
           </option>
         ))}
       </select>
-    </>
+    </Container>
   );
 };
 
