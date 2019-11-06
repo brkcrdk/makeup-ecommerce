4de513f5fb14c2c5672455caec6a93185faec6f2
Burak <brkcrdk@gmail.com>
2019-11-06 13:00:44 +0200

    Clearing

--- src/components/Shop/ProductList/ProductStyle.ts
+++ src/components/Shop/ProductList/ProductStyle.ts
@@ -1,23 +1,23 @@
 import styled from "styled-components";
 import { colours, device, fonts } from "../../utils";
 export const Container = styled.div`
-  /* box-shadow: ${colours.boxShadow}; */
   margin: 0 1em;
-  overflow:hidden;
-  h3{
-    text-align:center;
-    font-family:${fonts.montserrat};
-    font-size:1.5em;
+  overflow: hidden;
+  a {
+    text-decoration: none;
   }
-   @media ${device.mobileL}{
-    padding-right:0.5em;
+  h3 {
+    text-align: center;
+    font-family: ${fonts.montserrat};
+    font-size: 1.5em;
   }
-   @media ${device.laptop}{
-    padding-right:1em;
+  @media ${device.mobileL} {
+    padding-right: 0.5em;
   }
-  @media ${device.laptopL}{
-    padding-right:2em;
+  @media ${device.laptop} {
+    padding-right: 1em;
   }
-  /* display:grid; */
-  /* grid-template-columns:repeat(4,3fr) */
+  @media ${device.laptopL} {
+    padding-right: 2em;
+  }
 `;
