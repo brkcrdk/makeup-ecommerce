76a2ac271b9b1612e555d42b9a0eb7850ccaffec
Burak <brkcrdk@gmail.com>
2019-11-07 20:26:59 +0200

    Cart actions added

--- src/store/cart/reducer.ts
+++ src/store/cart/reducer.ts
@@ -7,7 +7,31 @@ const initialState: CartState = {
 const cartReducer = (state = initialState, action: CartActions) => {
   switch (action.type) {
     case ADD_CART:
-      return { ...state, cart: state.cart.concat(action.payload) };
+      if (state.cart.length >= 0) {
+        const id = state.cart.map((item) => {
+          return item.product.id;
+        });
+        if (id.indexOf(action.payload.product.id) === -1) {
+          return { ...state, cart: state.cart.concat(action.payload) };
+        } else {
+          const selected = id.indexOf(action.payload.product.id);
+          const newCount = action.payload.count;
+          const oldCount = state.cart[selected].count;
+          if (newCount > oldCount) {
+            return {
+              ...state,
+              cart: { ...state.cart, count: newCount + oldCount }
+            };
+          } else if (newCount < oldCount) {
+            return {
+              ...state,
+              cart: { ...state.cart, count: newCount }
+            };
+          } else {
+            return state;
+          }
+        }
+      }
     default:
       return state;
   }
