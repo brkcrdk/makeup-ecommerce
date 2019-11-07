37832119def78939bd7e816be1155ea8e29c5028
Burak <brkcrdk@gmail.com>
2019-11-07 16:02:04 +0200

    Cart store added

--- src/store/payload/action.ts
+++
@@ -1,9 +0,0 @@
-import { NameState, CHANGE_NAME } from "./types";
-
-const changeNameAction = (name: NameState) => {
-  return {
-    type: CHANGE_NAME,
-    payload: name
-  };
-};
-export default changeNameAction;
