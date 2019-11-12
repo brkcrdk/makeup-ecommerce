46c1467f929c22ce4ce05b6d2d996c99a4837201
Burak <brkcrdk@gmail.com>
2019-11-12 13:46:43 +0200

    localStorage action

--- src/store/index.ts
+++ src/store/index.ts
@@ -7,18 +7,20 @@ export type AppState = ReturnType<typeof rootReducer>;
 
 export default function configureStore() {
   const customMiddleware = (store: any) => (next: any) => (action: any) => {
-    const cart = store.getState().cartStore.cart;
-
     if (action.type === "ADD_CART") {
-      const oldCount = action.payload.count;
+      const cart = store.getState().cartStore.cart;
+      console.log(cart);
       const item = JSON.stringify(action.payload);
-      localStorage.setItem(`cart-${action.payload.product.id}`, item);
-      console.log(oldCount);
-      console.log(`current:${oldCount + action.payload.count}`);
+      if (localStorage.hasOwnPropery(`cart-${action.payload.product.id}`)) {
+        localStorage.setItem(`cart-${action.payload.product.id}`, item);
+      } else {
+        console.log("already exist");
+      }
+      // localStorage.setItem(`cart-${action.payload.product.id}`, item);
     } else if (action.type === "UPDATE_CART") {
-      localStorage.setItem("cart", JSON.stringify(action.payload));
+      // localStorage.setItem("cart", JSON.stringify(action.payload));
     } else if (action.type === "REMOVE_CART") {
-      localStorage.removeItem("cart");
+      // localStorage.removeItem("cart");
     }
     return next(action);
   };
