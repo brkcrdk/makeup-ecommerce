85a1629f6a032b72e7da665b0680101507fef9cb
Burak <brkcrdk@gmail.com>
2019-11-12 14:15:57 +0200

    update actions for localStorage

--- src/store/index.ts
+++ src/store/index.ts
@@ -9,6 +9,7 @@ export default function configureStore() {
   const customMiddleware = (store: any) => (next: any) => (action: any) => {
     const cart = store.getState().cartStore.cart;
     const ids = cart.map((item: any) => item.product.id);
+
     if (action.type === "ADD_CART") {
       const item = JSON.stringify(action.payload);
       if (`cart-${action.payload.product.id}` in localStorage) {
@@ -22,9 +23,25 @@ export default function configureStore() {
       } else {
         localStorage.setItem(`cart-${action.payload.product.id}`, item);
       }
-      // localStorage.setItem(`cart-${action.payload.product.id}`, item);
     } else if (action.type === "UPDATE_CART") {
-      // localStorage.setItem("cart", JSON.stringify(action.payload));
+      if (`cart-${action.payload.product.id}` in localStorage) {
+        const selected = ids.indexOf(action.payload.product.id);
+        const oldCount = cart[selected].count;
+        const update = action.payload.update;
+        if (update === "increment") {
+          const newItem = JSON.stringify({
+            product: action.payload.product,
+            count: oldCount + 1
+          });
+          localStorage.setItem(`cart-${action.payload.product.id}`, newItem);
+        } else if (update === "decrement") {
+          const newItem = JSON.stringify({
+            product: action.payload.product,
+            count: oldCount - 1
+          });
+          localStorage.setItem(`cart-${action.payload.product.id}`, newItem);
+        }
+      }
     } else if (action.type === "REMOVE_CART") {
       // localStorage.removeItem("cart");
     }
