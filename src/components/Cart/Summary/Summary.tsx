7527465e3c0caba2ebfc9bfa2bd3d0e4fa53d76d
Burak <brkcrdk@gmail.com>
2019-11-18 12:41:48 +0200

    Summary design change

--- src/components/Cart/Summary/Summary.tsx
+++ src/components/Cart/Summary/Summary.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import styled from "styled-components";
-import { device, fonts } from "../../utils";
+import { device, fonts, colours } from "../../utils";
 interface Props {}
 const Container = styled.div`
-  margin: 2em;
+  margin: 2em 0;
   @media ${device.mobileS} {
     padding: 0;
   }
@@ -15,8 +15,31 @@ const Container = styled.div`
     font-family: ${fonts.raleway};
   }
 `;
+const Info = styled.div`
+  display: grid;
+  text-align: center;
+  box-shadow: ${colours.boxShadow};
+  border-radius: 1em;
+  padding: 0.5em 1em;
+  span {
+    margin: 0 0.5em;
+  }
+`;
 const Summary: React.FC<Props> = () => {
-  return <Container>Summary goes here..</Container>;
+  return (
+    <Container>
+      <h3>Order Summary</h3>
+      <hr />
+      <Info>
+        <p>
+          Sub Total <span /> $ 123134
+        </p>
+        <p>
+          Sub Total <span /> $ 123134
+        </p>
+      </Info>
+    </Container>
+  );
 };
 
 export default Summary;
