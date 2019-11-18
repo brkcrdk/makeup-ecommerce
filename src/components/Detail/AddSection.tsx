140690ff6f5a8b762b880fc4d457132ab20192e8
Burak <brkcrdk@gmail.com>
2019-11-18 17:42:33 +0200

    Cleaning

--- src/components/Detail/AddSection.tsx
+++ src/components/Detail/AddSection.tsx
@@ -65,26 +65,23 @@ const Container = styled.div`
 `;
 const AddSection: React.FC<Props> = ({ product, price }) => {
   const [count, setCount] = useState(1);
-
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+  const handleDecrement = () => {
+    if (count > 1) return setCount(count - 1);
+  };
   return (
     <Container>
       <div>
         <h3>$ {price}</h3>
       </div>
       <div>
-        <button
-          onClick={() => {
-            setCount(count + 1);
-          }}>
+        <button onClick={handleIncrement}>
           <i className="fas fa-plus" />
         </button>
         <input value={count} readOnly />
-        <button
-          onClick={() => {
-            if (count > 1) {
-              setCount(count - 1);
-            }
-          }}>
+        <button onClick={handleDecrement}>
           <i className="fas fa-minus" />
         </button>
         <AddCart product={product} count={count} />
